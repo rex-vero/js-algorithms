50e7457fb88e2392633514b6d0587929f9720618
rex-vero <142157808+rex-vero@users.noreply.github.com>
2023-10-22 00:19:55 +0330

    maker multiples of 3

--- test7.js
+++ test7.js
@@ -1,6 +1,8 @@
 let readlineSync = require('readline-sync');
 let num1 = parseInt(readlineSync.question('first number: '));
 let num2 = parseInt(readlineSync.question('second number: '));
-for (let i = num1 + 3; i <= num2; i += 3) {
-    console.log(i);
+for (let i = num1; i <= num2; i++) {
+    if (i % 3 == 0) {
+        console.log(i);
+    }
 }
