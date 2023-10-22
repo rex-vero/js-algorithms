ab9247b54c90e613c8c82ae679320db3b49bc0b7
rex-vero <142157808+rex-vero@users.noreply.github.com>
2023-10-22 21:51:34 +0330

    bank info

---
+++ test14.js
@@ -0,0 +1,18 @@
+let readlineSync = require('readline-sync');
+let despointAndWithdrawal = parseInt(readlineSync.question('Enter 0 for despoint & 1 for withdrawal: '));
+const bankInfo = {
+    name: 'rex vero',
+    holding: 10000000,
+    resort: 15,
+    despoint: 100282,
+    withdrawal: 6354,
+}
+switch (despointAndWithdrawal) {
+    case 0:
+        console.log(`name: ${bankInfo.name} , holding: $${bankInfo.holding} , resort: ${bankInfo.resort} times , despoint: $${bankInfo.despoint}`);
+        break;
+
+    case 1:
+        console.log(`name: ${bankInfo.name} , holding: $${bankInfo.holding} , resort: ${bankInfo.resort} times , withdrawal: $${bankInfo.withdrawal}`);
+        break;
+}
