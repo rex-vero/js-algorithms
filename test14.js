aaa232678cbe9a66c3a99cc98d879359c3d1efae
rex-vero <142157808+rex-vero@users.noreply.github.com>
2023-10-22 22:04:04 +0330

    bank info

--- test14.js
+++ test14.js
@@ -1,5 +1,3 @@
-let readlineSync = require('readline-sync');
-let despointAndWithdrawal = parseInt(readlineSync.question('Enter 0 for despoint & 1 for withdrawal: '));
 const bankInfo = {
     name: 'rex vero',
     holding: 10000000,
@@ -7,13 +5,16 @@ const bankInfo = {
     despoint: 100282,
     withdrawal: 6354,
 }
+console.log(`name: ${bankInfo.name} , holding: $${bankInfo.holding} , resort: ${bankInfo.resort} times`);
+let readlineSync = require('readline-sync');
+let despointAndWithdrawal = parseInt(readlineSync.question('Enter 0 for despoint & 1 for withdrawal: '));
 switch (despointAndWithdrawal) {
     case 0:
-        console.log(`name: ${bankInfo.name} , holding: $${bankInfo.holding} , resort: ${bankInfo.resort} times , despoint: $${bankInfo.despoint}`);
+        console.log(`despoint: $${bankInfo.despoint}`);
         break;
 
     case 1:
-        console.log(`name: ${bankInfo.name} , holding: $${bankInfo.holding} , resort: ${bankInfo.resort} times , withdrawal: $${bankInfo.withdrawal}`);
+        console.log(`withdrawal: $${bankInfo.withdrawal}`);
         break;
     default:
         console.log('I said 0 or 1 you idiot >:( ');
